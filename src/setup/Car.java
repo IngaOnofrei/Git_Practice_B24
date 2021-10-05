c6f2070e8db4e4861aa649c15072d11272fd719b
Inga Onofrei <waterzart@gmail.com>
2021-10-05 13:43:48 -0400

    updating master to origin

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,16 +2,14 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this Car is in Master branch
 
     // this will cause a conflict with the james brnach
-=======
     //this is our car from james branch
 
     // some more tect
 
     // more txt
->>>>>>> james
 
 }
